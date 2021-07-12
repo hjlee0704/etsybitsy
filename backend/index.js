af3ea81effcee1de7626d1498eb486d53f519c2e
Anthony Lee <ahjlee74@gmail.com>
2021-07-12 15:58:35 -0700

    redis update

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
-
 require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const compression = require('compression');
+const redis = require('redis');
+
+const redisClient = redis.createClient(process.env.REDIS_URI);
+
 const { reviews } = require('../database');
 
 const avatarImgUrl = 'https://d1sa5mhlkaaod3.cloudfront.net/Archive/';
@@ -21,6 +24,24 @@ const getAverage = (shopReviews) => {
   return result;
 };
 
+const getShopReviews = (reviews) => {
+  reviews.row.map((review) => {
+    const singleReview = {
+      _id: id,
+      avatar: `${avatarImgUrl + review.avatar}.jpg`,
+      name: review.name,
+      date: review.date,
+      rating: review.rating,
+      description: review.description,
+      imageUrl: `${imgPath + review.imageurl}.jpg`,
+      purchasedItem: review.purchaseditem,
+      shopImage: `${imgPath + review.shopimage}.jpg`,
+    };
+    return singleReview;
+  });
+
+}
+
 const PUBLIC_DIR = path.resolve(__dirname, '../public');
 
 const app = express();
@@ -30,36 +51,39 @@ const PORT = process.env.PORT || 3001;
 // returns review with desired product id
 app.get('/api/reviews/:id', (req, res) => {
   const { id } = req.params;
-  reviews.query(`select * from reviews where product_id = ${id}`,
-    (err, data) => {
-      if (err) {
-        res.send(err);
-      }
-      const getReviews = {
-        _id: id,
-        shopReviews: [],
-        total: 0,
-        average: 1,
-      };
-      data.rows.map((review) => {
-        const singleReview = {
-          _id: id,
-          avatar: `${avatarImgUrl + review.avatar}.jpg`,
-          name: review.name,
-          date: review.date,
-          rating: review.rating,
-          description: review.description,
-          imageUrl: `${imgPath + review.imageurl}.jpg`,
-          purchasedItem: review.purchaseditem,
-          shopImage: `${imgPath + review.shopimage}.jpg`,
-        };
-        return getReviews.shopReviews.push(singleReview);
+  client.get(id, async (err, find_id) => {
+    if (err) throw err;
+
+    if (find_id) {
+      res.status(200).send({
+        jobs: json_decode( $id, TRUE ),
+        message: "data retrieved from the cache"
       });
-      getReviews.total = getTotal(getReviews.shopReviews);
-      getReviews.average = getAverage(getReviews.shopReviews);
-      res.send(getReviews);
-    });
-});
+    } else {
+
+      const retrieveReviews = reviews.query(`select * from reviews where product_id = ${id}`,
+      (err, data) => {
+
+        if (err) {
+          res.send(err);
+        }
+        const getReviews = {
+          _id: id,
+          shopReviews: [],
+          total: 0,
+          average: 1,
+        };
+        shopReview.push(getShopReviews(data));
+        getReviews.total = getTotal(getReviews.shopReviews);
+        getReviews.average = getAverage(getReviews.shopReviews);
+      });
+      client.setex(retrieveReviews, 600, json_decode( find_id, TRUE ));
+      res.status(200).send({
+        jobs: retrieveReviews,
+        message: "cache miss"
+      });
+    }
+  });
 
 app.use(compression());
 app.use(cors());
--- backend/newrelic.js
+++ backend/newrelic.js
@@ -6,6 +6,7 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
+  
   /**
    * Array of application names.
    */
@@ -13,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'newrelic',
+  license_key: process.env.newrelic,
   /**
    * This setting controls distributed tracing.
    * Distributed tracing lets you see the path that a request takes through your
